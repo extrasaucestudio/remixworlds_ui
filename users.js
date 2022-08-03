2f3b5fc5171d71bfd961195cbc2f3d09ea4c532a
slikhub <clonage@gmail.com>
2022-08-03 14:40:56 -0400

    yesss

---
+++ users.js
@@ -0,0 +1,17 @@
+var url_string = window.location.href;
+var url = new URL(url_string);
+var ref = url.searchParams.get("ref") || 'freecitygame';
+var toiframe = url.searchParams.get("url") || 'go.html';
+
+
+//var wallet = url.searchParams.get("wallet") || 'waxdashwaxio';
+//var wallet = ${wax.userAccount};
+
+//console.log(sessionStorage.getItem('username'));
+//console.log(sessionStorage.getItem('ref'));
+const affiliate_wallet = url.searchParams.get("ref") || 'freecitygame';
+
+//sessionStorage.setItem(wallet_) = ${wax.userAccount};
+//sessionStorage.getItem(wallet_);
+//console.log(ref);
+//console.log(sessionStorage.getItem('affiliate_wallet'));
